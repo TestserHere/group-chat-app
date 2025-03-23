b6dab01828bc2f455a789c76fefca4e6f28a6ee4
TestserHere <charlesmeng2012@gmail.com>
2025-03-23 13:10:36 +1100

    Updates

---
+++ precache-manifest.269e9b707041278720d0cd2ef45b6001.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5214d25d27a3460a4c93a88c876783f",
+    "url": "/group-chat-app/index.html"
+  },
+  {
+    "revision": "8ab7e50cbd373318506b",
+    "url": "/group-chat-app/static/css/main.2617b78f.chunk.css"
+  },
+  {
+    "revision": "6560f3ac18f7e6f32e8c",
+    "url": "/group-chat-app/static/js/2.57fa78e1.chunk.js"
+  },
+  {
+    "revision": "db4976e2c3fc371be79e",
+    "url": "/group-chat-app/static/js/3.1af95c37.chunk.js"
+  },
+  {
+    "revision": "8ab7e50cbd373318506b",
+    "url": "/group-chat-app/static/js/main.10519232.chunk.js"
+  },
+  {
+    "revision": "d5d548a96361f54699cd",
+    "url": "/group-chat-app/static/js/runtime~main.4c269fde.js"
+  }
+]);
